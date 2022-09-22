bc7837300b7812b3105255755596653b8627cce5
Ali <alifeyza4017@gmail.com>
2022-09-22 07:57:35 +0300

    added

--- app.js
+++ app.js
@@ -42,13 +42,13 @@ function displayTodos() {
     todos.forEach((todo) => {
         const todoItem = document.createElement("div")
         todoItem.classList.add("todo-item")
-        const label = document.querySelector("label")
-        const input = doc.querySelector("input")
-        const span = doc.querySelector("span")
-        const content = document.querySelector("content")
-        const actions = document.querySelector("actions")
-        const edit = document.querySelector("edit")
-        const deleteButton = document.querySelector("delete")
+        const label = document.createElement("label")
+        const input = document.createElement("input")
+        const span = document.createElement("span")
+        const content = document.createElement("content")
+        const actions = document.createElement("actions")
+        const edit = document.createElement("edit")
+        const deleteButton = document.createElement("delete")
 
         input.type = "checkbox"
         input.checked = todo.done
@@ -61,6 +61,58 @@ function displayTodos() {
         }
         content.classList.add("todo-content")
         actions.classList.add("todo-actions")
+        edit.classList.add("edit")
+        deleteButton.classList.add("delete")
+
+        content.innerHTML = `<input type="text" value="${todo.content}" readonly>`
+
+        edit.innerHTML= `Edit`
+        deleteButton.innerHTML = `Delete`
+        label.appendChild(input)
+        label.appendChild(span)
+        actions.appendChild(edit)
+        actions.appendChild(deleteButton)
+        todoItem.appendChild(label)
+        todoItem.appendChild(content)
+        todoItem.appendChild(actions)
+        todoList.appendChild(todoItem)
+
+        if(todo.done) {
+            todoItem.classList.add("done")
+        }
+
+        input.addEventListener("click", (e)=> {
+
+            todo.done = e.target.checked
+            localStorage.setItem("todos", JSON.stringify(todos))
+
+            if(todo.done) {
+                todoItem.classList.add("done")
+
+            }else {
+                todoItem.classList.remove("done")
+            }
+            displayTodos()
+
+        })
+
+        edit.addEventListener("click", (e)=> {
+            const input = content.querySelector("input")
+            input.removeAttribute("readonly")
+            input.focus()
+            input.addEventListener("blur", (e)=> {
+                input.setAttribute("readonly",true)
+                todo.content = e.target.value
+                localStorage.setItem("todos", JSON.stringify(todos))
+                displayTodos
+            })
+        })
+
+        deleteButton.addEventListener("click",(e)=> {
+            todos = todos.filter((t) => t != todo)
+            localStorage.setItem("todos", JSON.stringify(todos))
+            displayTodos()
+        })
     });
 
 }
